58d038db3b3cf9a125d5c5f7041e8f1866b08b08
Prateek Agarwal <pratekag@gmail.com>
2024-03-10 13:03:14 +0530

    Clonecli1, clone srv1: use s_sleep, register signal handlers, exit gracefully

--- examples/C++/clonecli1.cpp
+++ examples/C++/clonecli1.cpp
@@ -1,42 +1,47 @@
-#include <zmq.hpp>
+#include "zhelpers.hpp"
 #include <iostream>
 #include <unordered_map>
-
-// Narendra Pratap Singh - github.com/Narendra010/
+#include <zmq.hpp>
 
 using namespace std;
 
 int main() {
-	// Prepare our context and updates socket
-	zmq::context_t ctx(1);
-	zmq::socket_t updates(ctx, ZMQ_SUB);
-	updates.set(zmq::sockopt::subscribe, ""); // Subscribe to all messages
-	updates.connect("tcp://localhost:5555");
 
-	// Initialize key-value map and sequence
-	unordered_map<string, string> kvmap;
-	int64_t sequence = 0;
+  s_catch_signals();
+  // Prepare our context and updates socket
+  zmq::context_t ctx(1);
+  zmq::socket_t updates(ctx, ZMQ_SUB);
+  updates.set(zmq::sockopt::subscribe, ""); // Subscribe to all messages
+  updates.connect("tcp://localhost:5555");
 
-	while (true) {
-		// Receive key-value message
-		zmq::message_t kvmsg;
-		updates.recv(kvmsg, zmq::recv_flags::none);
+  // Initialize key-value map and sequence
+  unordered_map<string, string> kvmap;
+  int64_t sequence = 0;
 
-		// Convert message to string and extract key-value pair
-		string kvString(static_cast<char*>(kvmsg.data()), kvmsg.size());
-		size_t pos = kvString.find('=');
-		string key = kvString.substr(0, pos);
-		string value = kvString.substr(pos + 1);
-		cout << key << " --- " << value << endl;
-
-		// Store key-value pair in map
-		kvmap[key] = value;
-		sequence++;
+  while (!s_interrupted) {
+    // Receive key-value message
+    zmq::message_t kvmsg;
+    try {
+      (void)updates.recv(kvmsg, zmq::recv_flags::none);
+    }
+	catch(zmq::error_t& ex){
+		std::cerr << ex.what()<< std::endl;
+		break;
 	}
 
-	cout << "Interrupted" << endl;
-	cout << sequence << " messages in" << endl;
-	return 0;
-}
+    // Convert message to string and extract key-value pair
+    string kvString(static_cast<char *>(kvmsg.data()), kvmsg.size());
+    size_t pos = kvString.find('=');
+    string key = kvString.substr(0, pos);
+    string value = kvString.substr(pos + 1);
+    cout << key << " --- " << value << endl;
 
-// Narendra Pratap Singh - github.com/Narendra010/
+    // Store key-value pair in map
+    kvmap[key] = value;
+    sequence++;
+  }
+
+  cout << "Interrupted" << endl;
+  cout << sequence << " messages in" << endl;
+  return 0;
+}
--- examples/C++/clonesrv1.cpp
+++ examples/C++/clonesrv1.cpp
@@ -1,43 +1,43 @@
-#include <zmq.hpp>
-#include <Windows.h>
+#include "zhelpers.hpp"
 #include <iostream>
 #include <unordered_map>
-
-// Narendra Pratap Singh - github.com/Narendra010/
+#include <zmq.hpp>
 
 using namespace std;
 
 int main() {
-	// Prepare our context and publisher socket
-	zmq::context_t ctx(1);
-	zmq::socket_t publisher(ctx, ZMQ_PUB);
-	publisher.bind("tcp://*:5555");
-	Sleep(5000); // Sleep for a short while to allow connections to be established
+  // Prepare our context and publisher socket
+  s_catch_signals();
+  zmq::context_t ctx(1);
+  zmq::socket_t publisher(ctx, ZMQ_PUB);
+  publisher.bind("tcp://*:5555");
+  s_sleep(
+      5000); // Sleep for a short while to allow connections to be established
 
-	// Initialize key-value map and sequence
-	unordered_map<string,string> kvmap;
-	int64_t sequence = 0;
-	srand(time(NULL));
+  // Initialize key-value map and sequence
+  unordered_map<string, string> kvmap;
+  int64_t sequence = 0;
+  srand(time(NULL));
 
-	while (true) {
-		// Distribute as key-value message
-		zmq::message_t kvmsg(30); // Allocate enough space for key-value message
-		int key = rand() % 10000;
-		int body = rand() % 1000000;
-		snprintf((char *)kvmsg.data(), 30, "%d=%d", key, body); // Format key-value pair
-		publisher.send(kvmsg, zmq::send_flags::none); // Send key-value message
+  while (!s_interrupted) {
+    // Distribute as key-value message
+    zmq::message_t kvmsg; // Allocate enough space for key-value message
+    int key = rand() % 10000;
+    int body = rand() % 1000000;
+    std::string msg = std::to_string(key) + "=" + std::to_string(body);
+    kvmsg.rebuild(msg.size());
+    std::memcpy(kvmsg.data(), msg.c_str(), msg.size());
+    publisher.send(kvmsg, zmq::send_flags::none); // Send key-value message
 
-		// Store key-value pair in map
-		kvmap[to_string(key)] = to_string(body);
-		sequence++;
+    // Store key-value pair in map
+    kvmap[to_string(key)] = to_string(body);
+    sequence++;
 
-		// Sleep for a short while before sending the next message
-		Sleep(1000);
-	}
+    // Sleep for a short while before sending the next message
+    s_sleep(1000);
+  }
 
-	cout << "Interrupted" << endl;
-	cout << sequence << " messages out" << endl;
-	return 0;
+  cout << "Interrupted" << endl;
+  cout << sequence << " messages out" << endl;
+  return 0;
 }
-
-// Narendra Pratap Singh - github.com/Narendra010/
